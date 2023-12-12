87b6c3a4d40190424dbe6a4ff0ebf1b99ce694ff
techatikin <nikitakhabya03@gmail.com>
2023-12-12 13:13:48 +0530

    chore: minor changes

--- vanilla/index.js
+++ vanilla/index.js
@@ -18,7 +18,7 @@ async function getCommentDetails(selectedComment) {
     const userDetails = await fetch(`${BASE_URL}/users/${selectedComment.userId}`)
     const post = await postDetails.json()
     const user = await userDetails.json()
-    console.log(post, user)
+
     return { post, user }
   } catch (error) {
     console.error('Error finding resources', error);
@@ -62,12 +62,12 @@ async function handleSelectedComment(event) {
   const commentDetails = await reponse.json()
 
   // fetching post and user details
-  const selectedCommentDetails = await getCommentDetails(commentDetails)
-  renderCommentDetails(selectedCommentDetails, commentDetails)
+  const postAndUserDetails = await getCommentDetails(commentDetails)
+  renderCommentDetails(postAndUserDetails, commentDetails)
 }
 
 // rendering comment details
-const renderCommentDetails = (selectedCommentDetails, commentDetails) => {
+const renderCommentDetails = (postAndUserDetails, commentDetails) => {
   const container = document.querySelector('.preview')
   container.innerHTML = ''
 
@@ -75,19 +75,19 @@ const renderCommentDetails = (selectedCommentDetails, commentDetails) => {
   title.textContent = `Comment Preview - ${commentDetails.id}`
 
   const postTitle = document.createElement('h3')
-  postTitle.textContent = `Post: ${selectedCommentDetails.post.title}`
+  postTitle.textContent = `Post: ${postAndUserDetails.post.title}`
 
   const comment = document.createElement('p')
   comment.textContent = `Comment: ${commentDetails.comment}`
 
   const postBody = document.createElement('p')
-  postBody.textContent = `Post Content: ${selectedCommentDetails.post.content}`
+  postBody.textContent = `Post Content: ${postAndUserDetails.post.content}`
 
   const userName = document.createElement('p')
-  userName.textContent = `User Name: ${selectedCommentDetails.user.firstname} ${selectedCommentDetails.user.lastname}`
+  userName.textContent = `User Name: ${postAndUserDetails.user.firstname} ${postAndUserDetails.user.lastname}`
 
   const userEmail = document.createElement('p')
-  userEmail.textContent = `User Email: ${selectedCommentDetails.user.email}`
+  userEmail.textContent = `User Email: ${postAndUserDetails.user.email}`
 
   container.appendChild(title)
   container.appendChild(comment)
